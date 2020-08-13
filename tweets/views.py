8081c86342a6ff533f9d5f2b23884cfe6e63772d
Lukasz <lukaszkkoziel@gmail.com>
2020-08-13 10:27:39 +0900

    ajax

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
 import random
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
 
 from .forms import TweetForm
 from .models import Tweet
@@ -11,9 +11,14 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    print("next_url", next_url)
     if form.is_valid():
         obj = form.save(commit=False)
+        # do other form related logic
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
@@ -25,19 +30,15 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 10000)} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
     data = {
+        "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    """
-    REST API VIEW
-    Consume by JavaScript or Swift/Java/iOS/Andriod
-    return json data
-    """
     data = {
         "id": tweet_id,
     }
