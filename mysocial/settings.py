5661112576febb3719ae3dff0ac17943387650cc
Lukasz <lukaszkkoziel@gmail.com>
2020-08-14 11:28:04 +0900

    rest django

--- mysocial/settings.py
+++ mysocial/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 LOGIN_URL = "/login"
-
+MAX_TWEET_LENGTH = 240
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'tweets',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
