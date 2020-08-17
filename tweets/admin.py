04b41d45e700b4e6eab87214bf5e04dad48ad70a
Lukasz <lukaszkkoziel@gmail.com>
2020-08-18 08:06:37 +0900

    admin likes

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tweet, TweetLike
 
-from django.contrib import admin
 
-# Register your models here.
-from .models import Tweet
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
     class Meta:
