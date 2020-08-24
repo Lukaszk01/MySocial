4cc7b1861503abd104785ecab6539558698bd63f
Lukasz <lukaszkkoziel@gmail.com>
2020-08-24 09:05:47 +0900

    new tweets prints

--- mysocial-app/src/App.js
+++ mysocial-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {TweetsList} from './tweets'
+import {TweetsComponent} from './tweets'
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          <TweetsList />
+          <TweetsComponent />
         </div>
         <a
           className="App-link"
