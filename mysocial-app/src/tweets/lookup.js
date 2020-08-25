9d6cb56e4eb585a3c2f53e3342a687b53adf94cf
Lukasz <lukaszkkoziel@gmail.com>
2020-08-26 08:28:24 +0900

    api methods

---
+++ mysocial-app/src/tweets/lookup.js
@@ -0,0 +1,8 @@
+import {backendLookup} from '../lookup'
+export function apitweetCreate(newTweet, callback){
+    backendLookup("POST", "/tweets/create/", callback, {content: newTweet})
+  }
+  
+  export function apitweetList(callback) {
+      backendLookup("GET", "/tweets/", callback)
+  }
